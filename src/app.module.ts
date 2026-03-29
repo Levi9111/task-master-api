ceeca143d4311621cb15e3ad8bd0cf52743c917e
levi9111 <captainlevi9111@gmail.com>
2026-03-30 00:42:19 +0600

    refactor: update dependencies and restructure application modules
    - Added @nestjs/config, @nestjs/mongoose, and mongoose to dependencies.
    - Implemented configuration and MongoDB connection in AppModule.
    - Removed unused AppController and AppService files.
    - Updated main.ts to use nullish coalescing operator for PORT.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // load env variables
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Connect to DB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
