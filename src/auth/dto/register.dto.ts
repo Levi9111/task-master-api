8c201fbbd2201c9b75369222023ee438b03c450d
levi9111 <captainlevi9111@gmail.com>
2026-03-30 01:29:09 +0600

    feat: add authentication module with controller, service, and DTOs; enable global validation

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}
