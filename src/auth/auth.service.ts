9c47dca036f40da13a44d3f8444d554340f1f41a
levi9111 <captainlevi9111@gmail.com>
2026-03-30 20:36:19 +0600

    feat: implement JWT authentication strategy and protect user profile routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService, // injecting JWT service
+  ) {}
 
   async register(registerDto: RegisterDto) {
     const { name, email, password } = registerDto;
@@ -27,5 +36,33 @@ export class AuthService {
     delete userResponse.password;
 
     return userResponse;
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials!');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // Genrate JWT Payload
+    //"sub" (subject) is the standard JWT claim for the user identifier
+    const payload = {
+      sub: user._id,
+      email: user.email,
+      role: user.role,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
