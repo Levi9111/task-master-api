729eccc56090ef61720955f7ce25bb1b064f6562
levi9111 <captainlevi9111@gmail.com>
2026-03-30 20:09:34 +0600

    feat: implement user login functionality with JWT authentication in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth') //Sets the base route '/auth'
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('register') // '/auth/register'
   async register(@Body() reigsterDto: RegisterDto) {
     return this.authService.register(reigsterDto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK) // Changes default 201 Created to 200
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
