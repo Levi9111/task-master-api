fd6a8ad691c03905989764bfc495b01d3fa50891
levi9111 <captainlevi9111@gmail.com>
2026-04-10 18:56:55 +0600

    feat: implement comment creation and retrieval endpoints scoped to tasks

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { ParseObjectIdPipe } from 'src/common/pipes/parse-object-id.pipe';
+import { CreateCommentDto } from './dto/create-domment.dto';
 
-@Controller('comments')
-export class CommentsController {}
+@Controller('tasks/:taskId/comments')
+export class CommentsController {
+  constructor(private readonly commentService: CommentsService) {}
+
+  @Post()
+  async create(
+    @Param('taskId', ParseObjectIdPipe) taskId: string, // Extract and validate the Task ID from the URL
+    @Request() req, // Get the user from JWT
+    @Body() CreateCommentDto: CreateCommentDto,
+  ) {
+    return this.commentService.create(
+      taskId,
+      req.user.userId,
+      CreateCommentDto,
+    );
+  }
+
+  @Get()
+  async findAllByTask(@Param('taskId', ParseObjectIdPipe) taskId: string) {
+    return this.commentService.findByTask(taskId);
+  }
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Comment } from './schemas/comment.schema';
+import { Model } from 'mongoose';
+import { CreateCommentDto } from './dto/create-domment.dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectModel(Comment.name) private readonly commentModel: Model<Comment>,
+  ) {}
+
+  async create(
+    taskId: string,
+    userId: string,
+    createCommentDto: CreateCommentDto,
+  ): Promise<Comment> {
+    const newComment = new this.commentModel({
+      content: createCommentDto.content,
+      taskId,
+      authorId: userId,
+    });
+
+    const savedComment = await newComment.save();
+    return savedComment.populate('authorId', 'name email');
+  }
+
+  async findByTask(taskId: string): Promise<Comment[]> {
+    return this.commentModel
+      .find({ taskId })
+      .populate('authorId', 'name email')
+      .sort({ createdAt: -1 }) // newest comment at the top
+      .exec();
+  }
+}
